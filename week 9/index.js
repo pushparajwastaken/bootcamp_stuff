fd51fe4603158a91f796bcc1ca7c285ccbc11f36
Pushparaj <psinghparmar42@gmail.con>
2026-05-17 22:00:46 +0530

    fsafse

--- week 9/index.js
+++ week 9/index.js
@@ -1,24 +1,103 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const { authMiddleware } = require("./middleware");
+
 const app = express();
 app.use(express.json());
-app.use(express.json());
+
 const notes = [];
-//POST-create a note
-app.post("/notes", function (req, res) {
-  const note = req.body.note;
-  notes.push(note);
-  return res.json({
-    message: "Note added",
+const users = [
+  {
+    username: "harkirat",
+    password: "123123",
+  },
+];
+
+app.post("/signup", function (req, res) {
+  const username = req.body.username; // harkirat
+  const password = req.body.password;
+  const userExists = users.find((user) => user.username === username);
+  if (userExists) {
+    return res.status(403).json({
+      message: "User with this username already exists",
+    });
+  }
+
+  users.push({
+    username: username,
+    password: password,
+  });
+
+  res.json({
+    message: "You have signed up",
   });
 });
 
-//GET- get all my notes note
-app.get("/allnotes", function (req, res) {
-  return res.json({
-    notes: notes,
+app.post("/signin", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const userExists = users.find(
+    (user) => user.username === username && user.password === password,
+  );
+
+  if (!userExists) {
+    res.status(403).json({
+      message: "Incorrect credentials",
+    });
+    return;
+  }
+
+  // json web tokens
+  const token = jwt.sign(
+    {
+      username: username,
+    },
+    "harkirat123",
+  );
+
+  res.json({
+    token: token,
   });
 });
-app.get("/", function (req, res) {
-  res.sendFile("C:/Users/Asus/Videos/b/week 9/frontend/index.html");
+
+// POST - Create a note -- AUTHENTICATED ENDPOINT
+app.post("/notes", authMiddleware, function (req, res) {
+  const username = req.username;
+  const note = req.body.note;
+  notes.push({ note, username });
+
+  res.json({
+    message: "Done!",
+  });
 });
+
+// GET - get all my notes -- AUTHENTICATED ENDPOINT
+app.get("/notes", authMiddleware, function (req, res) {
+  const username = req.username;
+  const userNotes = notes.filter((note) => note.username === username);
+
+  res.json({
+    notes: userNotes,
+  });
+});
+
+app.get("/", function (req, res) {
+  res.sendFile(
+    "/Users/harkirat/Projects/bootcamp/week-9-notes-app/frontend/index.html",
+  );
+});
+
+app.get("/signup", function (req, res) {
+  res.sendFile(
+    "/Users/harkirat/Projects/bootcamp/week-9-notes-app/frontend/signup.html",
+  );
+});
+
+app.get("/signin", function (req, res) {
+  res.sendFile(
+    "/Users/harkirat/Projects/bootcamp/week-9-notes-app/frontend/signin.html",
+  );
+});
+
 app.listen(3000);
