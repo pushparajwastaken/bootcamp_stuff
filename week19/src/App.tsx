309f21cf363ae7638d1dfa746af67f53834294a3
Pushparaj <psinghparmar42@gmail.com>
2026-06-09 02:26:53 +0530

    add week 18

--- week19/src/App.tsx
+++ week19/src/App.tsx
@@ -1,24 +1,45 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+//react only re-renders components if a "state variable" changes
 function App() {
-  const posts = [
-    {
-      name: "Harkirat",
-      content: "Hi there",
-    },
-    {
-      name: "Pushparaj",
-      content: "Hi I am a learner",
-    },
-    {
-      name: "Lavanya",
-      content: "Hi I am the cutest",
-    },
-  ];
+  //only the first time this componenrt is rendered will data be initialized to []
+  //in all future re-renders,it will not get re-initialized
+  const [data, setData] = useState([{ title: "Hi there" }]);
+  // const posts = [
+  //   {
+  //     name: "Harkirat",
+  //     content: "Hi there",
+  //     backgroundColor: "blue",
+  //   },
+  //   {
+  //     name: "Pushparaj",
+  //     content: "Hi I am a learner",
+  //     backgroundColor: "yellow",
+  //   },
+  //   {
+  //     name: "Lavanya",
+  //     content: "Hi I am the cutest",
+  //     backgroundColor: "pink",
+  //   },
+  // ];
 
+  // setTimeout(() => {
+  //   setData([...data, { title: "New entry" }]);
+  // }, 1000);
+
+  //if the dependency array (2nd argument) is empty
+  //this function only runs on the first render(mount)
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos/")
+      .then((response) => {
+        setData(response.data);
+      });
+  }, []);
   return (
     <div>
-      <h1> LinkedIn!!!!!!!</h1>
-      {posts.map((p) => (
-        <Post name={p.name} content={p.content} />
+      {data.map((d) => (
+        <Post title={d.title} />
       ))}
     </div>
   );
@@ -27,6 +48,7 @@ function Post(props: any) {
   return (
     <div
       style={{
+        backgroundColor: props.backgroundColor,
         margin: 20,
         borderRadius: 20,
         padding: 20,
@@ -34,9 +56,9 @@ function Post(props: any) {
       }}
     >
       <div>
-        <b>{props.name}</b>
+        <b>{props.title}</b>
       </div>
-      <div>{props.content}</div>
+      {/* <div>{props.content}</div> */}
     </div>
   );
 }
